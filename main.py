8eb40a1f92fbcff72e4d02c61088bc4630f860b6
Mohammed-Salman-Fadel <msalmanfadel2004@gmail.com>
2025-09-19 21:08:38 +0300

    add schemas and prompt

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
 from dotenv import load_dotenv
-from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.agents import AgentExecutor
-from langchain.agents.react.agent import create_react_agent
 from langchain_tavily import TavilySearch
 from langchain import hub
+
+# langchain agents
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+
+# langchain_core
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
@@ -14,6 +23,13 @@ os.getenv("")
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"]
+)
+
 agent = create_react_agent(
     llm=llm,
     tools=tools,
@@ -26,7 +42,7 @@ chain = agent_executor
 def main():
     result = chain.invoke(
         input={
-            "input":"search for 3 ai engineering jobs using langchain in Singapore on LinkedIn and list their details."
+            "input":"search for 3 ai engineering jobs using langchain in New York on LinkedIn and list their details."
         }
     )
     print(result)
